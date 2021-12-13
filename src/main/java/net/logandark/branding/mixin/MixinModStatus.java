022cfb8ec0d40c7e33c03f8e75889921918193c7
Kichura <68134602+Kichura@users.noreply.github.com>
2021-12-13 17:45:21 +0100

    Fix mod status from revealing modded text.

---
+++ src/main/java/net/logandark/branding/mixin/MixinModStatus.java
@@ -0,0 +1,14 @@
+package net.logandark.branding.mixin;
+
+import net.minecraft.util.ModStatus;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ModStatus.class)
+public class MixinModStatus {
+
+	@Overwrite
+	public boolean isModded(){
+		return false;
+	}
+}
